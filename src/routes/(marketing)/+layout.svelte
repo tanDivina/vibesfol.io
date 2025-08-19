ddb1a2100e4f18ed2749872d1b9e6d5175f6867f
tanDivina <dorien.vda@gmail.com>
2025-08-18 21:43:41 -0500

    Update GitHub repository references to tanDivina/vibesfol.io

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -17,7 +17,7 @@
     <ul class="menu menu-horizontal px-1 hidden sm:flex font-bold text-lg">
       <li class="md:mx-4">
         <a
-          href="https://github.com/CriticalMoments/CMSaasStarter"
+          href="https://github.com/tanDivina/vibesfol.io"
           class="border border-primary">★ us on Github</a
         >
       </li>
@@ -68,7 +68,7 @@
         <li><a href="/search">Search</a></li>
         <li>
           <a
-            href="https://github.com/CriticalMoments/CMSaasStarter"
+            href="https://github.com/tanDivina/vibesfol.io"
             class="border border-primary">★ us on Github</a
           >
         </li>
@@ -99,7 +99,7 @@
       <nav>
         <h6 class="footer-title">Resources</h6>
         <a class="link link-hover" href="/blog">Blog</a>
-        <a class="link link-hover" href="https://github.com/CriticalMoments/CMSaasStarter">GitHub</a>
+        <a class="link link-hover" href="https://github.com/tanDivina/vibesfol.io">GitHub</a>
         <a class="link link-hover" href="/search">Search</a>
       </nav>
       <nav>
@@ -126,7 +126,7 @@
         </div>
         <div class="flex items-center gap-4 mt-4 md:mt-0">
           <span class="text-sm text-base-content/70">Built for developers, by developers</span>
-          <a href="https://github.com/CriticalMoments/CMSaasStarter" class="btn btn-ghost btn-sm">
+          <a href="https://github.com/tanDivina/vibesfol.io" class="btn btn-ghost btn-sm">
             <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 24 24">
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
             </svg>
