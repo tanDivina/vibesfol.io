9637eaee9c3bc5d54d95eef0794095f624b2a99d
tanDivina <dorien.vda@gmail.com>
2025-08-21 20:26:07 -0500

    Updated tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -49,6 +49,21 @@ module.exports = {
   ],
   daisyui: {
     themes: [
+      {
+        light: {
+          "primary": "#2563eb",
+          "secondary": "#9333ea", 
+          "accent": "#db2777",
+          "neutral": "#374151",
+          "base-100": "#ffffff",
+          "base-200": "#f9fafb",
+          "base-300": "#f3f4f6",
+          "info": "#3b82f6",
+          "success": "#10b981",
+          "warning": "#f59e0b",
+          "error": "#ef4444",
+        },
+      },
       "light",
       "dark",
     ],
