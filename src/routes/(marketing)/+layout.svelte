a967e8952de3fa3590bb4133cc9eece52311fee0
tanDivina <dorien.vda@gmail.com>
2025-08-21 21:34:20 -0500

    Restore original navbar styling

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -10,38 +10,36 @@
 </script>
 
 <div class="navbar bg-base-100 container mx-auto">
-  <div class="flex-1 justify-center">
-    <ul class="menu menu-horizontal px-6 hidden sm:flex font-semibold text-base gap-2">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl" href="/">{WebsiteName}</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1 hidden sm:flex font-bold text-lg">
       <li class="md:mx-4">
         <a
           href="https://github.com/tanDivina/vibesfol.io"
-          class="btn btn-outline btn-primary hover:btn-primary transition-all duration-200 shadow-sm">
-          <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 24 24">
-            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
-          </svg>
-          ★ Star on GitHub
+          class="border border-primary">★ us on Github
         </a>
       </li>
       <li class="md:mx-2">
-        <a href="/blog" class="hover:bg-base-200 rounded-lg px-4 py-2 transition-colors duration-200">
+        <a href="/blog">
           Blog
         </a>
       </li>
       <li class="md:mx-2">
-        <a href="/pricing" class="hover:bg-base-200 rounded-lg px-4 py-2 transition-colors duration-200">
+        <a href="/pricing">
           Pricing
         </a>
       </li>
       <li class="md:mx-2">
-        <a href="/account" class="hover:bg-base-200 rounded-lg px-4 py-2 transition-colors duration-200">
+        <a href="/account">
           Account
         </a>
-        >
       </li>
       <li class="md:mx-0">
-        <a href="/search" aria-label="Search" class="hover:bg-base-200 rounded-lg p-2 transition-colors duration-200">
+        <a href="/search" aria-label="Search">
           <svg
-            class="w-5 h-5 text-gray-600"
+            class="w-5 h-5"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -53,6 +51,8 @@
       </li>
     </ul>
     <div class="dropdown dropdown-end sm:hidden">
+      <!-- svelte-ignore a11y_label_has_associated_control -->
+      <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
       <!-- svelte-ignore a11y_label_has_associated_control -->
       <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
       <label tabindex="0" class="btn btn-ghost btn-circle">
@@ -70,6 +70,7 @@
           /></svg
         >
       </label>
+      <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
       <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
       <ul
         tabindex="0"
