debf98d16eb0bc4760206f738ae58daf0bc54a87
tanDivina <dorien.vda@gmail.com>
2025-08-27 01:11:05 -0500

    Updated vite.config.ts

--- src/routes/(public)/preview/[username]/_page.svelte
+++ src/routes/(public)/preview/[username]/_page.svelte
@@ -4,6 +4,7 @@
            No projects added yet. Go back to the editor to add some projects!
          </p>
 -        <a href="/build" class="btn btn-primary mt-4">
+        <a href="/build" class="btn btn-primary mt-4">
 +        <a href="/build" class="btn btn-primary mt-4">
            Add Projects
          </a>
--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,6 @@ import { buildAndCacheSearchIndex } from "./src/lib/build_index"
 export default defineConfig({
   plugins: [
     sveltekit(),
-    {
-      name: "vite-build-search-index",
-      writeBundle: {
-        order: "post",
-        sequential: false,
-        handler: async () => {
-          console.log("Building search index...")
-          await buildAndCacheSearchIndex()
-        },
-      },
-    },
   ],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
