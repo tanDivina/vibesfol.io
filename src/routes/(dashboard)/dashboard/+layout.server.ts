dce551e7c7657b5507a459c7b5f32493909f3088
tanDivina <dorien.vda@gmail.com>
2025-08-27 00:21:31 -0500

    Fix adapter configuration for static deployment

---
+++ src/routes/(dashboard)/dashboard/+layout.server.ts
@@ -0,0 +1,15 @@
+import { redirect } from "@sveltejs/kit"
+import type { LayoutServerLoad } from "./$types"
+
+export const load: LayoutServerLoad = async ({ locals: { safeGetSession } }) => {
+  const { session, user } = await safeGetSession()
+
+  if (!session || !user) {
+    throw redirect(303, "/login")
+  }
+
+  return {
+    session,
+    user,
+  }
+}
