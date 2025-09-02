1edaa3f0d07514eea920ffedc37864bb7913bfb2
tanDivina <dorien.vda@gmail.com>
2025-09-01 20:15:51 -0500

    Remove rollup-plugin-visualizer to fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vitest/config"
-import { visualizer } from "rollup-plugin-visualizer"
 
 export default defineConfig({
   plugins: [
     sveltekit(),
-    visualizer({
-      open: false,
-      filename: 'bundle-analysis.html',
-      gzipSize: true,
-      brotliSize: true,
-    }),
   ],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
